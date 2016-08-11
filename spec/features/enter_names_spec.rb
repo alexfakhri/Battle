2cbcc53c32c8f48e1853a7f249eae5addde95c78
Alex Fakhri <alexfakhri@me.com>
2016-08-11 12:28:52 +0100

    Added helper file to refactor tests

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,10 +4,7 @@ feature 'Enter names' do
 
   # As two Players, So we can play a personalised game of Battle, We want to Start a fight by entering our names and seeing them
   scenario 'submiting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Alex'
-    fill_in :player_2_name, with: 'Dave'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Alex Vs. Dave'
   end
 

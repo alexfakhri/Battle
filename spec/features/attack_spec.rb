6ff47c2670b96c6b675500ae09abdd785563518e
Alex Fakhri <alexfakhri@me.com>
2016-08-13 12:03:30 +0100

    Expanding the player class with hit points, 1 tests pending for reducing hit points

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -8,4 +8,12 @@ feature 'Attacking' do
     expect(page).to have_content 'Alex attacked Dave'
   end
 
+  # scenario 'reduce Player 2 HP by 10' do
+  #   sign_in_and_play
+  #   click_button 'Attack'
+  #   click_link 'OK'
+  #   expect(page).not_to have_content 'Dave: 60HP'
+  #   expect(page).to have_content 'Dave: 50HP'
+  # end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,4 +9,11 @@ describe Player do
       expect(alex.name).to eq 'Alex'
     end
   end
+
+  describe '#hit_points' do
+    it 'retunrs the hit points' do
+      expect(alex.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
 end

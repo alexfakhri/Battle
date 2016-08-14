06d483f67ad6219c6c1c9636ec221c44c473f4f6
Alex Fakhri <alexfakhri@me.com>
2016-08-14 18:46:38 +0100

    Refactored tests to use the web_helpers methods

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -10,26 +10,21 @@ feature 'Attacking' do
 
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     expect(page).not_to have_content 'Dave: 60HP'
     expect(page).to have_content 'Dave: 50HP'
   end
 
   scenario 'be attacke by player 2' do
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
+    attack_and_confirm
     click_button 'Attack'
     expect(page).to have_content "Dave attacked Alex"
   end
 
   scenario 'reduce player 1 HP by 10' do
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'OK'
-    click_button 'Attack'
-    click_button 'OK'
+    2.times { attack_and_confirm }
     expect(page).not_to have_content 'Alex: 60HP'
     expect(page).to have_content 'Alex: 50HP'
   end
